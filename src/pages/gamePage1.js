95e67f33cade9e2e12398664b11dc7484de22c21
uciyama886 <xneishan51@gmail.com>
2024-09-13 23:05:19 +0900

    fullScreeMode

--- src/pages/gamePage1.js
+++ src/pages/gamePage1.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment, useState } from "react";
 import Unity, { UnityContext } from "react-unity-webgl";
 
 const unityContext = new UnityContext({
@@ -8,6 +8,27 @@ const unityContext = new UnityContext({
     codeUrl: "Build/fuusenn.wasm",
 });
 function GamePage1() {
+
+    const [fsEvent, setFsEvent] = useState(false);
+
+    function Fullscreen() {
+        const unityInstance = unityContext.unityInstance;
+        if (!fsEvent) {
+            unityInstance.SetFullscreen(1);
+            setFsEvent(true);
+        }
+        else {
+            unityInstance.SetFullscreen(0);
+            setFsEvent(false);
+        }
+    }
+
+    window.addEventListener("keydown", function (event) {
+        if (event.key === "f") {
+            Fullscreen(fsEvent);
+        }
+    });
+
     return (
         <div>
             <Unity unityContext={unityContext} style={{
