654fe30f488da2d34f2b4bfb2fb5660584923e7f
uciyama886 <xneishan51@gmail.com>
2024-09-25 22:06:28 +0900

    design

--- src/pages/GameContainer1.js
+++ src/pages/GameContainer1.js
@@ -1,8 +1,53 @@
+// import React, { useState } from "react";
+// import { usePlayer } from "./PlayerConfig";
+// import GamePage1 from "./gamePage1";
+// import shot from "../images/shot.png";
+// import { useNavigate } from "react-router-dom";
+
+// function GameContainer1() {
+//     const [currentGameId, setCurrentGameId] = useState(0);
+//     const [showGamePage, setShowGamePage] = useState(false);
+//     const [playerNum] = usePlayer(); // usePlayerフックの使用
+//     const navigate = useNavigate();
+
+//     function showNewGameInstance() {
+//         setCurrentGameId(currentGameId + 1);
+//         setShowGamePage(true);
+//     }
+
+//     function navigateToLastPage() {
+//         navigate("/LastPage");
+//     };
+
+//     return (
+//         <div>
+//             <center>
+
+//                 {currentGameId !== playerNum ? (
+//                     <button onClick={showNewGameInstance}>ゲーム開始</button>
+//                 ) : (
+//                     <button onClick={navigateToLastPage} >次へ</button>
+//                 )}
+//                 {showGamePage && <GamePage1 key={currentGameId} id={currentGameId} />}
+//                 {currentGameId === 0 ? <img src={shot} width={500} alt="" /> : <div />}
+//             </center>
+//         </div>
+//     );
+// }
+// export default GameContainer1;
+
 import React, { useState } from "react";
 import { usePlayer } from "./PlayerConfig";
 import GamePage1 from "./gamePage1";
 import shot from "../images/shot.png";
 import { useNavigate } from "react-router-dom";
+import {
+    ChakraProvider,
+    Box,
+    Button,
+    Center,
+    Image
+} from "@chakra-ui/react";
 
 function GameContainer1() {
     const [currentGameId, setCurrentGameId] = useState(0);
@@ -20,18 +65,18 @@ function GameContainer1() {
     };
 
     return (
-        <div>
-            <center>
-
+        <ChakraProvider>
+            <Center flexDirection="column" mt="20px">
                 {currentGameId !== playerNum ? (
-                    <button onClick={showNewGameInstance}>ゲーム開始</button>
+                    <Button onClick={showNewGameInstance} mb="20px">ゲーム開始</Button>
                 ) : (
-                    <button onClick={navigateToLastPage} >次へ</button>
+                    <Button onClick={navigateToLastPage} mb="20px">次へ</Button>
                 )}
                 {showGamePage && <GamePage1 key={currentGameId} id={currentGameId} />}
-                {currentGameId === 0 ? <img src={shot} width={500} alt="" /> : <div />}
-            </center>
-        </div>
+                {currentGameId === 0 ? <Image src={shot} width={500} alt="" /> : <Box />}
+            </Center>
+        </ChakraProvider>
     );
 }
+
 export default GameContainer1;
--- src/pages/GameContainer2.js
+++ src/pages/GameContainer2.js
@@ -1,14 +1,21 @@
 import React, { useState } from "react";
 import { usePlayer } from "./PlayerConfig";
 import GamePage2 from "./gamePage2";
+import shot from "../images/shot.png";
 import { useNavigate } from "react-router-dom";
+import {
+    ChakraProvider,
+    Box,
+    Button,
+    Center,
+    Image
+} from "@chakra-ui/react";
 
 function GameContainer2() {
     const [currentGameId, setCurrentGameId] = useState(0);
     const [showGamePage, setShowGamePage] = useState(false);
-    const [playerNum] = usePlayer();
+    const [playerNum] = usePlayer(); // usePlayerフックの使用
     const navigate = useNavigate();
-
 
     function showNewGameInstance() {
         setCurrentGameId(currentGameId + 1);
@@ -17,19 +24,20 @@ function GameContainer2() {
 
     function navigateToLastPage() {
         navigate("/LastPage");
-    }
+    };
 
     return (
-        <div>
-            <center>
+        <ChakraProvider>
+            <Center flexDirection="column" mt="20px">
                 {currentGameId !== playerNum ? (
-                    <button onClick={showNewGameInstance}>ゲーム開始!</button>
+                    <Button onClick={showNewGameInstance} mb="20px">ゲーム開始</Button>
                 ) : (
-                    <button onClick={navigateToLastPage}>次へ</button>
+                    <Button onClick={navigateToLastPage} mb="20px">次へ</Button>
                 )}
                 {showGamePage && <GamePage2 key={currentGameId} id={currentGameId} />}
-            </center>
-        </div >
+                {currentGameId === 0 ? <Image src={shot} width={500} alt="" /> : <Box />}
+            </Center>
+        </ChakraProvider>
     );
 }
 export default GameContainer2;
