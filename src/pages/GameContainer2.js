fba520adaecebf03de20e809b3c9e092bfc37956
masa-massara <124395897+masa-massara@users.noreply.github.com>
2024-09-28 03:08:29 +0900

    キーボード操作をUnity内に限定

--- src/pages/GameContainer2.js
+++ src/pages/GameContainer2.js
@@ -3,41 +3,47 @@ import { usePlayer } from "./PlayerConfig";
 import GamePage2 from "./gamePage2";
 import shot from "../images/shot.png";
 import { useNavigate } from "react-router-dom";
-import {
-    ChakraProvider,
-    Box,
-    Button,
-    Center,
-    Image
-} from "@chakra-ui/react";
+import { ChakraProvider, Box, Button, Center, Image } from "@chakra-ui/react";
 
 function GameContainer2() {
-    const [currentGameId, setCurrentGameId] = useState(0);
-    const [showGamePage, setShowGamePage] = useState(false);
-    const [playerNum] = usePlayer(); // usePlayerフックの使用
-    const navigate = useNavigate();
+	const [currentGameId, setCurrentGameId] = useState(0);
+	const [showGamePage, setShowGamePage] = useState(false);
+	const [playerNum] = usePlayer();
+	const navigate = useNavigate();
 
-    function showNewGameInstance() {
-        setCurrentGameId(currentGameId + 1);
-        setShowGamePage(true);
-    }
+	function showNewGameInstance(e) {
+		if (e) e.target.blur(); // ボタンからフォーカスを外す
+		setCurrentGameId(currentGameId + 1);
+		setShowGamePage(true);
+	}
 
-    function navigateToLastPage() {
-        navigate("/LastPage");
-    };
+	function navigateToLastPage(e) {
+		if (e) e.target.blur(); // ボタンからフォーカスを外す
+		navigate("/LastPage");
+	}
 
-    return (
-        <ChakraProvider>
-            <Center flexDirection="column" mt="20px">
-                {currentGameId !== playerNum ? (
-                    <Button onClick={showNewGameInstance} mb="20px">ゲーム開始</Button>
-                ) : (
-                    <Button onClick={navigateToLastPage} mb="20px">次へ</Button>
-                )}
-                {showGamePage && <GamePage2 key={currentGameId} id={currentGameId} />}
-                {currentGameId === 0 ? <Image src={shot} width={500} alt="" /> : <Box />}
-            </Center>
-        </ChakraProvider>
-    );
+	return (
+		<ChakraProvider>
+			<Center flexDirection="column" mt="20px">
+				{currentGameId !== playerNum ? (
+					<Button onClick={(e) => showNewGameInstance(e)} mb="20px">
+						ゲーム開始
+					</Button>
+				) : (
+					<Button onClick={(e) => navigateToLastPage(e)} mb="20px">
+						次へ
+					</Button>
+				)}
+				{showGamePage && (
+					<GamePage2 key={currentGameId} id={currentGameId} />
+				)}
+				{currentGameId === 0 ? (
+					<Image src={shot} width={500} alt="" />
+				) : (
+					<Box />
+				)}
+			</Center>
+		</ChakraProvider>
+	);
 }
 export default GameContainer2;
--- src/pages/gamePage2.js
+++ src/pages/gamePage2.js
@@ -1,67 +1,87 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { usePlayer } from "./PlayerConfig";
 import Unity, { UnityContext } from "react-unity-webgl";
 import {
-    ChakraProvider,
-    Box,
-    Heading,
-    Button,
-    Center,
-    Text
+	ChakraProvider,
+	Box,
+	Heading,
+	Button,
+	Center,
+	Text,
 } from "@chakra-ui/react";
 
 const unityContext = new UnityContext({
-    loaderUrl: "Build/タイミングゲーム.loader.js",
-    dataUrl: "Build/タイミングゲーム.data",
-    frameworkUrl: "Build/タイミングゲーム.framework.js",
-    codeUrl: "Build/タイミングゲーム.wasm",
+	loaderUrl: "Build/タイミングゲーム.loader.js",
+	dataUrl: "Build/タイミングゲーム.data",
+	frameworkUrl: "Build/タイミングゲーム.framework.js",
+	codeUrl: "Build/タイミングゲーム.wasm",
 });
 
 function GamePage2({ id }) {
-    const [fsEvent, setFsEvent] = useState(false);
-    const [, , name] = usePlayer(); // usePlayerフックの使用
+	const [fsEvent, setFsEvent] = useState(false);
+	const [, , name] = usePlayer();
+	const unityContainerRef = useRef(null);
 
-    function Fullscreen() {
-        const unityInstance = unityContext.unityInstance;
-        if (!fsEvent) {
-            unityInstance.SetFullscreen(1);
-            setFsEvent(true);
-        } else {
-            unityInstance.SetFullscreen(0);
-            setFsEvent(false);
-        }
-    }
+	function Fullscreen() {
+		const unityInstance = unityContext.unityInstance;
+		if (!fsEvent) {
+			unityInstance.SetFullscreen(1);
+			setFsEvent(true);
+		} else {
+			unityInstance.SetFullscreen(0);
+			setFsEvent(false);
+		}
+	}
 
-    useEffect(() => {
-        const handleKeyDown = (event) => {
-            if (event.key === "f" || event.key === "F") {
-                Fullscreen();
-            }
-        };
+	useEffect(() => {
+		const handleKeyDown = (event) => {
+			if (event.key === "f" || event.key === "F") {
+				Fullscreen();
+			}
+		};
 
-        window.addEventListener("keydown", handleKeyDown);
-        return () => {
-            window.removeEventListener("keydown", handleKeyDown);
-        };
-    });
+		window.addEventListener("keydown", handleKeyDown);
 
-    return (
-        <ChakraProvider>
-            <Center flexDirection="column" mt="20px">
-                <Text fontSize="xl" mb="10px">No.{id}</Text>
-                <Heading as="h2" size="lg" mb="10px">{name[id - 1]}さん</Heading>
-                <Button onClick={Fullscreen} mb="20px">
-                    {fsEvent ? "フルスクリーン解除" : "Fボタン:フルスクリーン"}
-                </Button>
-                <Box border="2px solid black" bg="gray.200" p="10px">
-                    <Unity unityContext={unityContext} style={{
-                        height: "60vh",
-                        width: 700,
-                    }} />
-                </Box>
-            </Center>
-        </ChakraProvider>
-    );
+		if (unityContainerRef.current) {
+			unityContainerRef.current.focus();
+		}
+
+		return () => {
+			window.removeEventListener("keydown", handleKeyDown);
+		};
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [fsEvent]);
+
+	return (
+		<ChakraProvider>
+			<Center flexDirection="column" mt="20px">
+				<Text fontSize="xl" mb="10px">
+					No.{id}
+				</Text>
+				<Heading as="h2" size="lg" mb="10px">
+					{name[id - 1]}さん
+				</Heading>
+				<Button onClick={Fullscreen} mb="20px">
+					{fsEvent ? "フルスクリーン解除" : "Fボタン:フルスクリーン"}
+				</Button>
+				<Box
+					ref={unityContainerRef}
+					tabIndex="0"
+					border="2px solid black"
+					bg="gray.200"
+					p="10px"
+				>
+					<Unity
+						unityContext={unityContext}
+						style={{
+							height: "60vh",
+							width: 700,
+						}}
+					/>
+				</Box>
+			</Center>
+		</ChakraProvider>
+	);
 }
 
 export default GamePage2;
